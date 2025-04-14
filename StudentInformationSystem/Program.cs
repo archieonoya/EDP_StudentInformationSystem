646a44b059d77376c862577770a7d98829746b7d
Archie Onoya <archieonoya13@gmail.com>
2025-04-14 12:38:39 +0800

    added login form

--- StudentInformationSystem/Program.cs
+++ StudentInformationSystem/Program.cs
@@ -16,7 +16,7 @@ namespace StudentInformationSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
