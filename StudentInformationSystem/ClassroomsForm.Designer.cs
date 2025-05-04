1e263ffc44bf7c4e48009345cc258de27158df36
Archie Onoya <archieonoya13@gmail.com>
2025-05-04 23:57:30 +0800

    updated ClassroomForm with backend

--- StudentInformationSystem/ClassroomsForm.Designer.cs
+++ StudentInformationSystem/ClassroomsForm.Designer.cs
@@ -61,6 +61,7 @@
             this.btnAdd.TabIndex = 111;
             this.btnAdd.Text = "Add Classroom";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // dgvClassrooms
             // 
@@ -116,6 +117,7 @@
             this.btnDashboard.TabIndex = 22;
             this.btnDashboard.Text = "Dashboard";
             this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
             // 
             // btnLogout
             // 
@@ -130,6 +132,7 @@
             this.btnLogout.TabIndex = 21;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
             // btnManageStudents
             // 
@@ -144,6 +147,7 @@
             this.btnManageStudents.TabIndex = 12;
             this.btnManageStudents.Text = "Manage Students";
             this.btnManageStudents.UseVisualStyleBackColor = false;
+            this.btnManageStudents.Click += new System.EventHandler(this.btnManageStudents_Click);
             // 
             // btnViewLogs
             // 
@@ -158,6 +162,7 @@
             this.btnViewLogs.TabIndex = 20;
             this.btnViewLogs.Text = "View Logs";
             this.btnViewLogs.UseVisualStyleBackColor = false;
+            this.btnViewLogs.Click += new System.EventHandler(this.btnViewLogs_Click);
             // 
             // btnManageProfessor
             // 
@@ -172,6 +177,7 @@
             this.btnManageProfessor.TabIndex = 13;
             this.btnManageProfessor.Text = "Manage Professors";
             this.btnManageProfessor.UseVisualStyleBackColor = false;
+            this.btnManageProfessor.Click += new System.EventHandler(this.btnManageProfessor_Click);
             // 
             // btnViewStats
             // 
@@ -186,6 +192,7 @@
             this.btnViewStats.TabIndex = 19;
             this.btnViewStats.Text = "View Enrollment Stats";
             this.btnViewStats.UseVisualStyleBackColor = false;
+            this.btnViewStats.Click += new System.EventHandler(this.btnViewStats_Click);
             // 
             // btnManageDepartments
             // 
@@ -200,6 +207,7 @@
             this.btnManageDepartments.TabIndex = 14;
             this.btnManageDepartments.Text = "Manage Departments";
             this.btnManageDepartments.UseVisualStyleBackColor = false;
+            this.btnManageDepartments.Click += new System.EventHandler(this.btnManageDepartments_Click);
             // 
             // btnViewEnrollments
             // 
@@ -214,6 +222,7 @@
             this.btnViewEnrollments.TabIndex = 18;
             this.btnViewEnrollments.Text = "View Enrollments";
             this.btnViewEnrollments.UseVisualStyleBackColor = false;
+            this.btnViewEnrollments.Click += new System.EventHandler(this.btnViewEnrollments_Click);
             // 
             // btnManageClassrooms
             // 
@@ -228,6 +237,7 @@
             this.btnManageClassrooms.TabIndex = 15;
             this.btnManageClassrooms.Text = "Manage Classrooms";
             this.btnManageClassrooms.UseVisualStyleBackColor = false;
+            this.btnManageClassrooms.Click += new System.EventHandler(this.btnManageClassrooms_Click);
             // 
             // btnManageSchedules
             // 
@@ -242,6 +252,7 @@
             this.btnManageSchedules.TabIndex = 17;
             this.btnManageSchedules.Text = "Manage Schedules";
             this.btnManageSchedules.UseVisualStyleBackColor = false;
+            this.btnManageSchedules.Click += new System.EventHandler(this.btnManageSchedules_Click);
             // 
             // btnManageSubjects
             // 
@@ -256,6 +267,7 @@
             this.btnManageSubjects.TabIndex = 16;
             this.btnManageSubjects.Text = "Manage Subjects";
             this.btnManageSubjects.UseVisualStyleBackColor = false;
+            this.btnManageSubjects.Click += new System.EventHandler(this.btnManageSubjects_Click);
             // 
             // txtCapacity
             // 
--- StudentInformationSystem/ClassroomsForm.cs
+++ StudentInformationSystem/ClassroomsForm.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace StudentInformationSystem
 {
     public partial class ClassroomsForm : Form
     {
+        private int loggedInAdminId = 1; // Placeholder for admin_id; replace with actual session value
+
         public ClassroomsForm()
         {
             InitializeComponent();
@@ -19,7 +16,157 @@ namespace StudentInformationSystem
 
         private void ClassroomsForm_Load(object sender, EventArgs e)
         {
+            LoadClassrooms();
+        }
 
+        private void LoadClassrooms()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT building, room_number, capacity FROM classrooms";
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                    {
+                        System.Data.DataTable dt = new System.Data.DataTable();
+                        adapter.Fill(dt);
+                        dgvClassrooms.DataSource = dt;
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Database error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBuilding.Text) ||
+                string.IsNullOrWhiteSpace(txtRoomNumber.Text) ||
+                string.IsNullOrWhiteSpace(txtCapacity.Text))
+            {
+                MessageBox.Show("Please fill in all required fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!int.TryParse(txtCapacity.Text, out int capacity) || capacity <= 0)
+            {
+                MessageBox.Show("Capacity must be a positive number.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string insertQuery = "INSERT INTO classrooms (building, room_number, capacity) VALUES (@building, @room_number, @capacity)";
+                    using (MySqlCommand cmd = new MySqlCommand(insertQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@building", txtBuilding.Text);
+                        cmd.Parameters.AddWithValue("@room_number", txtRoomNumber.Text);
+                        cmd.Parameters.AddWithValue("@capacity", capacity);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Classroom added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtBuilding.Text = "";
+                    txtRoomNumber.Text = "";
+                    txtCapacity.Text = "";
+                    LoadClassrooms();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Database error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            DashboardForm dashboardForm = new DashboardForm();
+            dashboardForm.Show();
+            this.Hide();
+        }
+
+        private void btnManageStudents_Click(object sender, EventArgs e)
+        {
+            StudentsForm studentsForm = new StudentsForm();
+            studentsForm.Show();
+            this.Hide();
+        }
+
+        private void btnManageProfessor_Click(object sender, EventArgs e)
+        {
+            ProfessorsForm professorsForm = new ProfessorsForm();
+            professorsForm.Show();
+            this.Hide();
+        }
+
+        private void btnManageDepartments_Click(object sender, EventArgs e)
+        {
+            DepartmentsForm departmentsForm = new DepartmentsForm();
+            departmentsForm.Show();
+            this.Hide();
+        }
+
+        private void btnManageClassrooms_Click(object sender, EventArgs e)
+        {
+            // Already on ClassroomsForm, no action needed
+        }
+
+        private void btnManageSubjects_Click(object sender, EventArgs e)
+        {
+            SubjectsForm subjectsForm = new SubjectsForm();
+            subjectsForm.Show();
+            this.Hide();
+        }
+
+        private void btnManageSchedules_Click(object sender, EventArgs e)
+        {
+            SchedulesForm schedulesForm = new SchedulesForm();
+            schedulesForm.Show();
+            this.Hide();
+        }
+
+        private void btnViewEnrollments_Click(object sender, EventArgs e)
+        {
+            EnrollmentsForm enrollmentsForm = new EnrollmentsForm();
+            enrollmentsForm.Show();
+            this.Hide();
+        }
+
+        private void btnViewStats_Click(object sender, EventArgs e)
+        {
+            EnrollmentStatsForm statsForm = new EnrollmentStatsForm();
+            statsForm.Show();
+            this.Hide();
+        }
+
+        private void btnViewLogs_Click(object sender, EventArgs e)
+        {
+            LogsForm logsForm = new LogsForm();
+            logsForm.Show();
+            this.Hide();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Close();
         }
     }
 }
